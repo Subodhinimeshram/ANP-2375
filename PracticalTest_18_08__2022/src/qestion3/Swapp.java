f36132191d8387c0cfc28a78d23e949028018fd7
SUBODHINI <SUBODHINI@DESKTOP-J384RGO>
2022-08-18 21:45:04 +0530

    PracticalTest_18_08_2022

---
+++ PracticalTest_18_08__2022/src/qestion3/Swapp.java
@@ -0,0 +1,17 @@
+package qestion3;
+
+public class Swapp {
+	 
+	    public static void main(String args[]){
+	    	
+	        int a =5,b=2;
+	        
+	        a = a + b;
+	        b = a- b;
+	        a = a - b;
+	        
+	        System.out.println("After swapping a and b is :" + " a = " + a + ", b = " + b);
+	    }
+	}
+
+
